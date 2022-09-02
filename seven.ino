17e3ccd2c71ee07f5e209ffa9dff3cf12796447a
Romashka <rroma13579@gmail.com>
2022-09-02 22:43:48 +0200

    third commit

---
+++ seven.ino
@@ -0,0 +1,77 @@
+#define Pin1 7
+#define Pin2 6
+#define Pin3 5
+#define Pin4 4
+#define Pin5 2
+#define Pin6 11
+#define Pin7 10
+#define Pin8 9
+#define Pin9 8
+#define Pin10 3
+
+void setup () {
+
+  pinMode (Pin10,OUTPUT);
+ pinMode (Pin1,OUTPUT);
+  pinMode (Pin2,OUTPUT); 
+  pinMode (Pin3,OUTPUT);
+   pinMode (Pin4,OUTPUT);
+    pinMode (Pin5,OUTPUT);
+     pinMode (Pin6,OUTPUT);
+      pinMode (Pin7,OUTPUT);
+     pinMode (Pin8,OUTPUT);
+     pinMode (Pin9,OUTPUT);
+  
+}
+
+void loop () {
+  
+while (true){
+digitalWrite(Pin1,LOW);
+digitalWrite(Pin2,LOW);
+digitalWrite(Pin3,LOW);
+digitalWrite(Pin4,HIGH);
+digitalWrite(Pin5,LOW);
+digitalWrite(Pin6,LOW);
+digitalWrite(Pin7,HIGH);
+digitalWrite(Pin8,HIGH);
+digitalWrite(Pin9,LOW);
+digitalWrite(Pin10,LOW);
+delay(10);
+digitalWrite(Pin1,HIGH);
+digitalWrite(Pin2,LOW);
+digitalWrite(Pin3,HIGH);
+digitalWrite(Pin4,LOW);
+digitalWrite(Pin5,LOW);
+digitalWrite(Pin6,LOW);
+digitalWrite(Pin7,HIGH);
+digitalWrite(Pin8,HIGH);
+digitalWrite(Pin9,LOW);
+digitalWrite(Pin10,LOW);
+delay(10);
+digitalWrite(Pin1,HIGH);
+digitalWrite(Pin2,LOW);
+digitalWrite(Pin3,HIGH);
+digitalWrite(Pin4,LOW);
+digitalWrite(Pin5,LOW);
+digitalWrite(Pin6,LOW);
+digitalWrite(Pin7,HIGH);
+digitalWrite(Pin8,HIGH);
+digitalWrite(Pin9,LOW);
+digitalWrite(Pin10,LOW);
+delay(10);
+digitalWrite(Pin1,HIGH);
+digitalWrite(Pin2,LOW);
+digitalWrite(Pin3,HIGH);
+digitalWrite(Pin4,LOW);
+digitalWrite(Pin5,LOW);
+digitalWrite(Pin6,LOW);
+digitalWrite(Pin7,HIGH);
+digitalWrite(Pin8,HIGH);
+digitalWrite(Pin9,LOW);
+digitalWrite(Pin10,LOW);
+delay(10);
+
+
+}
+}
